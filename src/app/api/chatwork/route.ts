9e7bd52a3e949a45b7341284e39da39942f59923
shimafumi2010 <support@ai-pc.net>
2025-10-08 08:50:22 +0900

    Delete src/app/api/chatwork directory

--- src/app/api/chatwork/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { NextResponse } from 'next/server'
-
-export async function POST(req: Request) {
-  const { body } = await req.json()
-  const r = await postChatworkMessage(body)
-  return NextResponse.json({ ok: true, r })
-}
