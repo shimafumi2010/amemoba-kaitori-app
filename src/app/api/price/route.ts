380419234c2542341737e33da75f4c79dc0359f2
shimafumi2010 <support@ai-pc.net>
2025-10-08 08:54:28 +0900

    Delete src directory

--- src/app/api/price/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { fetchAmemobaPriceByQuery } from '@/lib/amemoba'
-
-export async function POST(req: Request) {
-  const { query } = await req.json()
-  const price = await fetchAmemobaPriceByQuery(query)
-  return NextResponse.json({ price })
-}
